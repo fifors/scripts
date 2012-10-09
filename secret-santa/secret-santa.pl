0c7e3af277c00063d53ac8869f169e7afdc462b8
Peter Buckingham <peter_buckingham@dell.com>
2012-10-09 14:55:45 -0700

    Updating secret-santa.pl to support sending html based email. Auto generate the mailing list for reponse emails. Add support for not having duplicate givers/kks from year to year. Some general formatting improvements.

--- secret-santa/secret-santa.pl
+++ secret-santa/secret-santa.pl
@@ -5,9 +5,7 @@ use warnings;
 use strict;
 use Getopt::Std;
 use Email::Send;
-use Email::Sender::Simple qw(sendmail);
-use Email::Sender::Transport::SMTP::TLS;
-use Email::Simple::Creator;
+use Email::MIME;
 use Users;
 
 sub usage {
@@ -20,10 +18,11 @@ sub usage {
     print "-M\t\tSend email messages\n";
     print "-a\t\tShow all the details\n";
     print "-f file\tFile with list of names, eg peter,\tpeter\@asdf.com\n";
+    print "-d file\tFile with list of duplicate names\n";
     print "-n names\tList of names to use\n";
     print "-s Santa\tEmail address to use for the Secret Santa\n";
-    print "-A auditor\tAuditor's email";
-    print "-h\t\tPrints this help message\n"
+    print "-A auditor\tAuditor's email\n";
+    print "-h\t\tPrints this help message\n";
 }
 
 my %options;
@@ -31,6 +30,8 @@ my @names;
 my @partners;
 my %contacts;
 my $file = "names";
+my $dups = "dups";
+my %dupl;
 my $ssanta = "ssanta\@ragnarak.com";
 my $auditor;
 local *INPUT;
@@ -42,7 +43,7 @@ my $showall = 0;
 
 %options = ();
 
-getopts("mMaf:n:F:e:s:A:h", \%options);
+getopts("mMaf:n:F:e:s:A:hd:", \%options);
 
 if (defined($options{"h"})) {
     usage();
@@ -50,38 +51,43 @@ if (defined($options{"h"})) {
 }
 
 if (defined($options{"f"})) {
-	print "Using \"$options{'f'}\" as the source of names\n";
-	$file = $options{"f"};
+    print "Using \"$options{'f'}\" as the source of names\n";
+    $file = $options{"f"};
 }
 
 if (defined($options{"s"})) {
-	print "Using $ssanta as the email of the Secret Santa\n";
-	$ssanta = $options{"s"};
+    print "Using $ssanta as the email of the Secret Santa\n";
+    $ssanta = $options{"s"};
 }
 
 if (defined($options{"A"})) {
-	print "Using \"$options{'A'}\" as the email of the auditor\n";
-	$auditor = $options{"A"};
+    print "Using \"$options{'A'}\" as the email of the auditor\n";
+    $auditor = $options{"A"};
 }
 if (defined($options{"a"})) {
-	print "WARNING: showing all details\n";
-	$showall = 1;
+    print "WARNING: showing all details\n";
+    $showall = 1;
+}
+
+if (defined($options{"d"})) {
+    print "Using \"$options{'d'}\" as the source of duplicates\n";
+    $dups = $options{"d"};
 }
 
 if (-f $file) {
     open(INPUT, $file);
     while (<INPUT>) {
-	chomp;
-	if (! -z $_) {
-	    my ($n, $nn, $e, $a) = split(/,/);
-            $e =~ s/\s+//g;
-            $a =~ s/(\b)  (\b)/$1\n$2/g;
-            $a =~ s/\s+\b//;
+        chomp;
+        if (!-z $_) {
+            my ($n, $nn, $e, $a) = split(/,/);
+            $e  =~ s/\s+//g;
+            $a  =~ s/(\b)  (\b)/$1<br>\n$2/g;
+            $a  =~ s/\s+\b//;
             $nn =~ s/\s+//g;
-	    $contacts{$n} = [$e, $a, $nn];
-	    push(@names, $n);
-	    push(@partners, $n);
-	}
+            $contacts{$n} = [ $e, $a, $nn ];
+            push(@names,    $n);
+            push(@partners, $n);
+        }
     }
     close(INPUT);
 } else {
@@ -89,29 +95,69 @@ if (-f $file) {
     exit 1;
 }
 
-my $pass = "kk-gmail.txt";
+if (-f $dups) {
+    open(INPUT, $dups);
+    while (<INPUT>) {
+        chomp;
+        if (!-z $_) {
+            my ($n, $d) = split(/,/);
+            $dupl{$n} = $d;
+            $d =~ s/^\s+//;
+            $d =~ s/\s+$//;
+            #print "name: $n\tdup: $d\n";
+        }
+    }
+    close(INPUT);
+} else {
+    print "There are no dups\n";
+    exit 1;
+}
+#my $pass  = "kk-gmail.txt";
+my $pass  = "godaddy.txt";
+#my $pass  = "gmail.txt";
 my $users = new Users($pass);
-my %info = %{ $users->{_values} };
+my %info  = %{ $users->{_values} };
 
 while ($#partners >= 0) {
     $index = int(rand($#partners));
 
-    for (my $i = 0; ($partners[$index] eq $names[$pers]); $i++) {
-	if ($i == 5) {
-	    $pers--;
-	    push(@partners, $ss{$names[$pers]});
-	    next;
-	}
-	$index = int(rand($#partners));
+    for (my $i = 0 ; (($partners[$index] eq $names[$pers])) ; $i++) {
+        if ($i == 5) {
+            $pers--;
+            push(@partners, $ss{ $names[$pers] });
+            next;
+        }
+        $index = int(rand($#partners));
+        my $dup = "none";
+
+        if (defined($dupl{ $names[$pers] })) {
+            $dup = $dupl{ $names[$pers] };
+        }
+
+        #print "Partner: $partners[$index]\tname: $names[$pers]\tdup: $dup\n";
+        if ($dup =~ /$partners[$index]/) {
+            print "Fixing duplicate, old index: $index\n";
+            $index = int(rand($#partners));
+            for (my $i = 0 ; (($partners[$index] eq $dup)) ; $i++) {
+                if ($i == 5) {
+                    $pers--;
+                    push(@partners, $ss{ $names[$pers] });
+                    last;
+                }
+                $index = int(rand($#partners));
+            }
+
+            print "new index: $index\n";
+        }
     }
 
     $el = splice(@partners, $index, 1);
-    $ss{$names[$pers]} = $el;
+    $ss{ $names[$pers] } = $el;
     $pers++;
 }
 
 if ($showall) {
-    foreach  (sort (keys (%ss))) {
+    foreach (sort (keys (%ss))) {
         print "$_: $ss{$_}\n";
     }
     print "\n\n";
@@ -126,74 +172,81 @@ if (defined($options{"M"})) {
     chomp($ans);
 
     if ($ans eq 'y') {
-	$sendMail = $options{"M"};
+        $sendMail = $options{"M"};
     } else {
-	print "Not sending the mail, printing it out instead\n";
-	$printMail = "y";
+        print "Not sending the mail, printing it out instead\n";
+        $printMail = "y";
     }
 }
+
+# Write email list
+local *OUT;
+my $o = "mail_list.txt";
+
+my $idx = 0;
+my %reply;
+open(OUT, ">$o");
+foreach my $k (sort (keys (%contacts))) {
+    print OUT "$contacts{$k}[0],$contacts{$ss{$k}}[0]\n";
+    my $m;
+    foreach my $j (sort (keys (%ss))) {
+        if ($ss{$j} eq $k) {
+            $m = $j;
+            last;
+        }
+    }
+
+    print OUT "$contacts{$k}[0],$contacts{$m}[0]\n";
+
+    $reply{ $contacts{$k} } = $idx;
+
+    $idx += 2;
+}
+close(OUT);
 
 if ($printMail || $sendMail) {
-    foreach  (sort (keys (%contacts))) {
-	my $from = "Secret Santa <$ssanta>";
-	my $to = "$_ <$contacts{$_}[0]>";
-	my $subject = "Your Kris Kindle is....";
-	my $msg = "\n$contacts{$ss{$_}}[2]\n\nDo _not_ reveal under pain of death!\n\nThe mailing address for $ss{$_} is:\n\n$contacts{$ss{$_}}[1]\n\nSecret Santa\n";
-	my $email = Email::Simple->create(
-	    header => [
-		From    => $from,
-		To      => $to,
-		Subject => $subject,
+    foreach (sort (keys (%contacts))) {
+        my $from    = "Secret Santa <$ssanta>";
+        my $to      = "$_ <$contacts{$_}[0]>";
+        my $subject = "Your Kris Kindle is....";
+        my $msg =
+          "$contacts{$ss{$_}}[2]<br><br>Do _not_ reveal under any circumstance!";
+
+        $msg .= "<br><br>The mailing address for $ss{$_} is:<br><br>";
+        $msg .= "$contacts{$ss{$_}}[1]<br><br>Secret Santa<br><br>";
+
+        my $ref = $reply{ $contacts{$_} };
+
+        $msg .= "Click <a href=\"http://ragnarak.com/ss.cgi?id=$ref\">here</a>";
+        $msg .= " when sending your gift.<br>";
+
+        $ref++;
+        $msg .= "Click <a href=\"http://ragnarak.com/ss.cgi?id=$ref\">here</a>";
+        $msg .= " when you've recieved your gift.<br>";
+
+        my $email = Email::MIME->create(
+	    attributes => {
+		content_type => 'multipart/alternative',
+	    },
+            header_str => [
+                From    => $from,
+                To      => $to,
+                Subject => $subject,
+            ],
+	    parts => [
+		Email::MIME->create(
+		    attributes 	=> { content_type => 'text/plain' },
+            body => $msg,
+		),
+		Email::MIME->create(
+		    attributes 	=> { content_type => 'text/html' },
+		    body	=> $msg,
+		)
 	    ],
-	    body => $msg,
-	    );
+       );
 
-	if ($printMail) {
-            print "To: $to\n";
-            print "From: $from\n";
-            print "Subject: $subject\n";
-            print "Msg: $msg\n";
-        }
-
-        if ($sendMail) {
-	    my $transport = Email::Sender::Transport::SMTP::TLS->new(
-		host => 'smtp.gmail.com',
-		port => 587,
-		username => $info{'user'},
-		password => $info{'pass'},
-		helo => 'ragnarak.com',
-		);
-
-	    sendmail($email, {transport => $transport});
-	    die "Error sending email: $@" if $@;
-        }
-    }
-}
-
-if (defined($auditor)) {
-	my $from = "Secret Santa <$ssanta>";
-	my $to = "$auditor";
-	my $subject = "Kris Kindle List";
-	my $msg;
-
-	$msg = "Kris Kindles\nSecret Santa:\tfor:\n";
-	$msg = $msg . "--------------------\n";
-	foreach  (sort (keys (%ss))) {
-	    $msg = $msg . "$_\t\t$ss{$_}\n";
-	}
-	$msg = $msg . "\nThanks!\n\nSecret Santa\n";
-
-	my $email = Email::Simple->create(
-	    header => [
-		From    => $from,
-		To      => $to,
-		Subject => $subject,
-	    ],
-	    body => $msg,
-	    );
-
-	if ($printMail) {
-            print "To: $to\n";
+        if ($printMail) {
+            print "\nTo: $to\n";
             print "From: $from\n";
             print "Subject: $subject\n";
             print "Msg: $msg\n";
@@ -201,14 +254,62 @@ if (defined($auditor)) {
 
         if ($sendMail) {
             my $send = Email::Send->new(
-		{  mailer => 'Gmail',
-		   mailer_args => [
-		        username => $info{'user'},
-		        password => $info{'pass'},
-		   ]
+		{ mailer => 'SMTP',
+		  mailer_args => [
+                Host     => $info{'host'},
+                ssl      => 1,
+                username => $info{'user'},
+                password => $info{'pass'},
+		  ]
 		}
-	    );
+            );
+
 	    eval { $send->send($email) };
-	    die "Error sending email: $@" if $@;
+            die "Error sending email: $@" if $@;
         }
+    }
+}
+
+if (defined($auditor)) {
+    my $from    = "Secret Santa <$ssanta>";
+    my $to      = "$auditor";
+    my $subject = "Kris Kindle List";
+    my $msg;
+
+    $msg = "Kris Kindles\nSecret Santa:\tfor:\n";
+    $msg = $msg . "--------------------\n";
+    foreach (sort (keys (%ss))) {
+        $msg = $msg . "$_\t\t$ss{$_}\n";
+    }
+    $msg = $msg . "\nThanks!\n\nSecret Santa\n";
+
+    my $email = Email::Simple->create(
+        header => [
+            From    => $from,
+            To      => $to,
+            Subject => $subject,
+        ],
+        body => $msg,
+   );
+
+    if ($printMail) {
+        print "To: $to\n";
+        print "From: $from\n";
+        print "Subject: $subject\n";
+        print "Msg: $msg\n";
+    }
+
+    if ($sendMail) {
+        my $send = Email::Send->new(
+            {
+                mailer      => 'Gmail',
+                mailer_args => [
+                    username => $info{'user'},
+                    password => $info{'pass'},
+                ]
+            }
+       );
+        eval { $send->send($email) };
+        die "Error sending email: $@" if $@;
+    }
 }
